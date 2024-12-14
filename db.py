48d5abbd3964d27a4dd5adac7f6030aefefa6638
Optimusgt5 <anish.kapare1@gmail.com>
2024-12-15 00:47:14 +1100

    fixed multitude of problems where the login page didnt work

---
+++ db.py
@@ -0,0 +1,35 @@
+import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
+
+def GetDB():
+
+    # Connect to the database and return the connection object
+    db = sqlite3.connect(".database/gmr.db")
+    db.row_factory = sqlite3.Row
+
+    return db
+
+def GetAllGuesses():
+
+    # Connect, query all guesses and then return the data
+    db = GetDB()
+    guesses = db.execute("SELECT * FROM Guesses").fetchall()
+    db.close()
+    return guesses
+
+def CheckLogin(username, password):
+
+    db = GetDB()
+
+    # Ask the database for a single user matching the provided name
+    user = db.execute("SELECT * FROM Users WHERE username=?", (username,)).fetchone()
+
+    # Do they exist?
+    if user is not None:
+        # OK they exist, is their password correct
+        if check_password_hash(user['password'], password):
+            # They got it right, return their details 
+            return user
+        
+    # If we get here, the username or password failed.
+    return None
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 import db
 
 app = Flask(__name__)
@@ -6,6 +6,28 @@ app.secret_key = "gtg"
 
 @app.route("/")
 def Home():
-    return render_template("index.html")
+    guessData = db.GetAllGuesses()
+    return render_template("index.html", guesses=guessData)
+
+@app.route("/login", methods=["GET", "POST"])
+def Login():
+    # They sent us data, get the username and password
+    # then check if their details are correct.
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+
+        # Did they provide good details
+        user = db.CheckLogin(username, password)
+        if user:
+            # Yes! Save their username and id then
+            session['username'] = username
+            session['id'] = user['id']
+
+
+            # Send them back to the homepage
+            return redirect("/")
+    return render_template("login.html")
+
 
 app.run(debug=True, port=5000)
